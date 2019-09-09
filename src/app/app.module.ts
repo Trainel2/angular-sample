ae3897bde76260952a87963c339ecbad18db0d88
Benten <t.paparn@gmail.com>
2019-09-09 16:55:09 +0700

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ScrollToTopComponent } from './layout/scroll-to-top/scroll-to-top.component';
+import { LogoutModalComponent } from './layout/logout-modal/logout-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    SidebarComponent
+    SidebarComponent,
+    TopbarComponent,
+    FooterComponent,
+    ScrollToTopComponent,
+    LogoutModalComponent
   ],
   imports: [
     BrowserModule,
